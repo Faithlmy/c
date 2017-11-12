6d1ef6edd0f60d2389526d758f08d4168c90fe33
Faithlmy <lmengyy@126.com>
2017-11-12 23:56:10 +0800

    c

---
+++ src/Fprintf.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+main()
+{
+	int fa;
+	for( fa = 0; fa <= 10; fa++)
+		printf("%3d %6.1f\n",fa, 5.9*fa);
+}
---
+++ src/fputchar.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	char c = 'B';
+	putchar(c);
+	printf("\n");
+	return 0;
+}
